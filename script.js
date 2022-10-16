8933f2ff965dad3e72b84f344a4809933c504e00
Dmytro Skrynnyk <dmytroskrynnyk@MacBook-Pro-Dmytro.local>
2022-10-16 23:33:20 +0300

    Create work

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 document.body.innerHTML = '<h1>Hello QA Team</h1>'
 console.log('Test !!!!!!!!!!!!!!')
+function sayHi() {
+    alert('Hello from Git');
+}
+
+function sayBye() {
+    alert('Goodbye from Git');
+}
